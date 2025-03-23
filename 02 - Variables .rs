d710eac8036f70ac41494d419f702b1e94c8e200
Nima hoseini <khodehnj@gmail.com>
2025-03-23 23:45:18 +0330

    Add files via upload

---
+++ 02 - Variables .rs
@@ -0,0 +1,8 @@
+// Variables in Rust
+fn main() {
+    let x = 10;      // Immutable variable
+    let mut y = 20;  // Mutable variable
+    y = 30;          // Changing value of y
+    println!("x: {}, y: {}", x, y);
+}
+// Output: x: 10, y: 30
